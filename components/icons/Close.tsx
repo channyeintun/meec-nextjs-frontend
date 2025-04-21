2282cc5d662c06b401fd645892c84bcd1d9c687a
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2025-04-21 17:32:33 +0700

    feat: add icons

---
+++ components/icons/Close.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+const Close = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="none"
+        viewBox="0 0 16 16"
+    >
+        <path fill="#fff" fillOpacity="0.01" d="M0 0h16v16H0z"></path>
+        <path
+            fill="#161616"
+            d="m12 4.7-.7-.7L8 7.3 4.7 4l-.7.7L7.3 8 4 11.3l.7.7L8 8.7l3.3 3.3.7-.7L8.7 8z"
+        ></path>
+    </svg>
+);
+
+export default Close;
---
+++ components/icons/Menu.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+const Menu = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="none"
+        viewBox="0 0 16 16"
+    >
+        <path fill="#fff" fillOpacity="0.01" d="M0 0h16v16H0z"></path>
+        <path
+            fill="#161616"
+            d="M14 3H2v1h12zM14 12H2v1h12zM14 6H2v1h12zM14 9H2v1h12z"
+        ></path>
+    </svg>
+);
+
+export default Menu;
