96d2a24213a22ef6aa4352ea663db9ccf3c8d5d8
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2025-04-24 07:43:31 +0700

    refactor

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,8 +33,6 @@ export const Navbar = () => {
                             animate={{ height: "auto", opacity: 1 }}
                             exit={isDesktop ? { height: 0, opacity: 0 } : { height: "auto", opacity: 1 }}
                             transition={{ duration: 0.3, ease: "easeInOut" }}
-                            // @ts-ignore
-                            style={{ anchorName: "--header" }}
                         >
                             {/* overlay */}
                             <div className="w-full h-full bg-[var(--overlay)] absolute inset-0 transition-transform duration-300 ease-in-out lg:hidden -translate-x-full group-has-[:checked]:translate-x-0 z-20"></div>
@@ -61,7 +59,7 @@ export const Navbar = () => {
 
                 {/* Navigation Links */}
                 <nav className="lg:flex lg:items-center lg:justify-start">
-                    <ul className="nav-links lg:ps-[var(--spacing-05)] group-has-[:checked]:flex lg:flex lg:flex-row flex-col absolute top-full left-0 w-full lg:relative border-b border-[var(--border-subtle-00)] bg-[var(--background)] transition-transform duration-300 ease-in-out max-lg:-translate-x-full group-has-[:checked]:translate-x-0">
+                    <ul className="nav-links lg:ps-[var(--spacing-05)] group-has-[:checked]:flex lg:flex lg:flex-row flex-col absolute top-full left-0 w-full lg:relative border-b border-[var(--border-subtle-00)] bg-[var(--background)] transition-transform duration-300 ease-in-out max-lg:-translate-x-full group-has-[:checked]:translate-x-0 h-[calc(100dvh-58px)]">
                         <li>
                             <Link onNavigate={menuHandlers.close} href="/" className={cn(
                                 "block max-md:!px-4 max-lg:!px-8 max-lg:!py-[14px] max-lg:border-b border-[var(--border-subtle-00)] carbon-button",
