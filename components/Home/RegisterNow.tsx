5219df973f82cc9d7ab0ccbf11dd31a3f3ad179f
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2025-04-26 21:35:49 +0700

    fix: update icon size

--- components/Home/RegisterNow.tsx
+++ components/Home/RegisterNow.tsx
@@ -7,8 +7,8 @@ export const RegisterNowLink = () => {
     return (
         <Link className="sm:absolute sm:bottom-0 sm:end-0 grid heading-01 transition-all duration-200 ease-[cubic-bezier(0.47,0,0.23,1.38)] bg-[var(--layer-01)] hover:bg-[var(--layer-hover-01)] text-[var(--text-primary)] h-[160px] sm:w-[320px] p-[var(--spacing-05)] group border border-[var(--border-subtle-00)]" href="/">
             <span>{t("Register now")}</span>
-            <div className="w-8 h-8 mt-auto">
-                <ArrowRight className="w-8 h-8 group-hover:translate-x-full transition-all duration-200 ease-[cubic-bezier(0.47,0,0.23,1.38)]" />
+            <div className="w-6 h-6 mt-auto">
+                <ArrowRight className="w-6 h-6 group-hover:translate-x-full transition-all duration-200 ease-[cubic-bezier(0.47,0,0.23,1.38)]" />
             </div>
         </Link>);
 }
