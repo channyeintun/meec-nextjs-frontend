9169a7ccb103d650a4cabb79d6b294f3e9908627
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2025-04-26 20:43:28 +0700

    fix: update bg color

--- components/Home/YoutubeSection.tsx
+++ components/Home/YoutubeSection.tsx
@@ -6,7 +6,7 @@ export const YoutubeSection = () => {
     const t = useTranslations("HomePage");
 
     return (
-        <section className="px-[var(--spacing-07)] py-[var(--spacing-10)] flex justify-center">
+        <section className="px-[var(--spacing-07)] py-[var(--spacing-10)] flex justify-center bg-[#D0E2FF]">
             <div className="space-y-[var(--spacing-09)] lg:max-w-[640px]">
                 <p className="fluid-paragraph-01">{t("Youtube description")}</p>
                 <Link className="w-fit font-sans text-base tracking-[0] leading-[22px] flex items-center gap-8 px-4 py-[13px] sm:pt-[29px] bg-[var(--button-primary)] hover:bg-[var(--button-primary-hover)] hover:text-[--text-inverse] text-[var(--text-on-color)]" href="https://www.youtube.com" target="_blank">
