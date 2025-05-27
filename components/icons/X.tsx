ba5e83a113f778ca5ade1707a69f018bcd3ac946
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2025-05-28 00:51:40 +0700

    fix: update icon colors

--- components/icons/X.tsx
+++ components/icons/X.tsx
@@ -22,7 +22,7 @@ const X = ({ className }: IconProps) => (
             style={{ mixBlendMode: "multiply" }}
         ></path>
         <path
-            fill="#161616"
+            fill="light-dark(#161616,#ffffff)"
             d="M13.676 10.622 20.233 3h-1.554l-5.693 6.618L8.438 3H3.194l6.876 10.007L3.194 21h1.553l6.013-6.989L15.562 21h5.245zm-2.129 2.474-.696-.997-5.544-7.93h2.387l4.474 6.4.696.996 5.816 8.319h-2.387z"
         ></path>
     </svg>
