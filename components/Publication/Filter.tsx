ad64c7450092c79859e07c51c7f5c7b51f1ddf15
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2025-05-29 23:55:06 +0700

    refactor

--- components/Publication/Filter.tsx
+++ components/Publication/Filter.tsx
@@ -1,14 +1,11 @@
 'use client';
 
-import { useRouter } from "@/i18n/navigation";
-import { useSearchParams } from "next/navigation";
-import { FC } from "react";
-import Checked from "../icons/Checked";
-import Unchecked from "../icons/Unchecked";
-import { useDisclosure } from "@mantine/hooks";
-import ChevronDown from "../icons/ChevronDown";
 import { cn } from "@/lib/utils";
 import { Author } from "@/types/article";
+import { useDisclosure } from "@mantine/hooks";
+import { FC } from "react";
+import { Checkbox } from "../common/Checkbox";
+import ChevronDown from "../icons/ChevronDown";
 
 export const FilterDropdown: FC<{
     authors: Author[];
@@ -64,54 +61,3 @@ export const AuthorFilter: FC<{
             </div>
         )
     }
-
-const Checkbox: FC<{
-    value: Author;
-    filterKey: string;
-}> = ({ value, filterKey }) => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-
-    const currentFilters = searchParams.getAll(filterKey);
-    const isChecked = currentFilters.includes(value.slug);
-
-    const toggleFilter = () => {
-        const params = new URLSearchParams(searchParams.toString());
-        const newFilters = isChecked
-            ? currentFilters.filter((filter) => filter !== value.slug)
-            : [...currentFilters, value.slug];
-
-        params.delete(filterKey);
-
-        newFilters.forEach((filter) => {
-            params.append(filterKey, filter);
-        });
-
-        router.push(`?${params.toString()}`, {
-            scroll: false
-        });
-    };
-
-    return (
-        <label
-            htmlFor={value.slug}
-            className="flex items-center gap-1 text-[var(--text-primary)] cursor-pointer transition-colors  [&:has(input[type=checkbox]:focus-visible)]:ring-2 [&:has(input[type=checkbox]:focus-visible)]:ring-blue-600 [&:has(input[type=checkbox]:focus-visible)]:ring-offset-2"
-        >
-            <input
-                type="checkbox"
-                id={value.slug}
-                checked={isChecked}
-                onChange={toggleFilter}
-                className="absolute sr-only"
-            />
-            {isChecked ? (
-                <Checked aria-hidden="true" className="min-w-5 min-h-5" />
-            ) : (
-                <Unchecked aria-hidden="true" className="min-w-5 min-h-5" />
-            )}
-            <span className="label-01">
-                {value.name} ({value.articles?.length ?? 0})
-            </span>
-        </label>
-    );
-};
---
+++ components/common/Checkbox.tsx
@@ -0,0 +1,62 @@
+import { useRouter } from "@/i18n/navigation";
+import { useSearchParams } from "next/navigation";
+import { FC } from "react";
+import Checked from "../icons/Checked";
+import Unchecked from "../icons/Unchecked";
+
+interface Value {
+    name: string;
+    slug: string;
+    articles?: any[];
+}
+
+export const Checkbox: FC<{
+    value: Value;
+    filterKey: string;
+}> = ({ value, filterKey }) => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const currentFilters = searchParams.getAll(filterKey);
+    const isChecked = currentFilters.includes(value.slug);
+
+    const toggleFilter = () => {
+        const params = new URLSearchParams(searchParams.toString());
+        const newFilters = isChecked
+            ? currentFilters.filter((filter) => filter !== value.slug)
+            : [...currentFilters, value.slug];
+
+        params.delete(filterKey);
+
+        newFilters.forEach((filter) => {
+            params.append(filterKey, filter);
+        });
+
+        router.push(`?${params.toString()}`, {
+            scroll: false
+        });
+    };
+
+    return (
+        <label
+            htmlFor={value.slug}
+            className="flex items-center gap-1 text-[var(--text-primary)] cursor-pointer transition-colors  [&:has(input[type=checkbox]:focus-visible)]:ring-2 [&:has(input[type=checkbox]:focus-visible)]:ring-blue-600 [&:has(input[type=checkbox]:focus-visible)]:ring-offset-2"
+        >
+            <input
+                type="checkbox"
+                id={value.slug}
+                checked={isChecked}
+                onChange={toggleFilter}
+                className="absolute sr-only"
+            />
+            {isChecked ? (
+                <Checked aria-hidden="true" className="min-w-5 min-h-5" />
+            ) : (
+                <Unchecked aria-hidden="true" className="min-w-5 min-h-5" />
+            )}
+            <span className="label-01">
+                {value.name} ({value.articles?.length ?? 0})
+            </span>
+        </label>
+    );
+};
