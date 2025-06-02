b5edea579ce6dcb43e15379a947104ee0ca10776
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2025-06-02 17:10:29 +0700

    fix: update accessibility

--- components/Blog/Share.tsx
+++ components/Blog/Share.tsx
@@ -1,9 +1,9 @@
 import { Link } from "@/i18n/navigation";
+import { cn } from "@/lib/utils";
 import { FC } from "react";
 import Facebook from "../icons/Facebook";
 import LinkedIn from "../icons/LinkedIn";
 import X from "../icons/X";
-import { cn } from "@/lib/utils";
 
 export const Share: FC<{
     url: string;
@@ -14,13 +14,37 @@ export const Share: FC<{
 }) => {
 
         return (
-            <div className={cn("border-t boder-[var(--border-strong-01)] flex flex-col gap-[var(--spacing-05)] pt-[var(--spacing-05)] pb-[var(--spacing-08)]", className)}>
-                <span className="text-[var(--text-primary)] label-01">Share on</span>
-                <div className="flex gap-[var(--spacing-05)]">
-                    <Link target="_blank" href={`https://www.facebook.com/share_channel/?type=reshare&link=${url}&app_id=966242223397117&source_surface=external_reshare&display&hashtag#`}><Facebook /></Link>
-                    <Link target="_blank" href={`https://www.linkedin.com/shareArticle?mini=true&url=${url}`}><LinkedIn /></Link>
-                    <Link target="_blank" href={`https://twitter.com/intent/tweet?url=${url}`}><X /></Link>
-                </div>
-            </div>
+            <nav className={cn("border-t boder-[var(--border-strong-01)] flex flex-col gap-[var(--spacing-05)] pt-[var(--spacing-05)] pb-[var(--spacing-08)]", className)}>
+                <h2 className="text-[var(--text-primary)] label-01">Share on</h2>
+                <ul className="flex gap-[var(--spacing-05)]">
+                    <li>
+                        <Link
+                            target="_blank"
+                            href={`https://www.facebook.com/share_channel/?type=reshare&link=${url}&app_id=966242223397117&source_surface=external_reshare&display&hashtag#`}
+                            aria-label="Share on Facebook"
+                        >
+                            <Facebook />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            target="_blank"
+                            href={`https://www.linkedin.com/shareArticle?mini=true&url=${url}`}
+                            aria-label="Share on LinkedIn"
+                        >
+                            <LinkedIn />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            target="_blank"
+                            href={`https://twitter.com/intent/tweet?url=${url}`}
+                            aria-label="Share on X (Twitter)"
+                        >
+                            <X />
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         )
     }
